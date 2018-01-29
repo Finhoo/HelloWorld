3687b9304f4eb0c696f81d8c83ba4fe25977bd8d
Jinhao <fjinhao@qq.com>
2018-01-29 16:24:08 +0800

    Initial commit

---
+++ app/src/main/java/com/example/fanjinhao/helloworld/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.fanjinhao.helloworld;
+
+
+import android.os.Bundle;
+
+
+public class MainActivity extends com.fayne.aartest.MainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
+
